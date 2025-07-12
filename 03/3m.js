b2c1f608736361370f430aba4a4e4a8b7de94079
stoicshreay <shreyank2024@gmail.com>
2025-07-12 06:59:10 +0000

    some changes to acknowledge

--- 03/3m.js
+++ 03/3m.js
@@ -1,30 +1,36 @@
+// const { createElement } = require("react")
+
 const bd=document.body
 const f1=document.getElementById("f1")
 const i1=document.getElementById("i1")
 const i2=document.getElementById("i2")
 const b1=document.getElementById("b1")
-
+let newele1 = document.createElement('ul')
+document.body.appendChild(newele1)
 b1.addEventListener("click",(e)=>{
-    event.preventDefault();
+    e.preventDefault();
     let h=i1.value
     let w=i2.value
     // let bmi = ((w/h)/h)*10000
     let bmi=w*h
     console.log(bmi)
     // console.log(typeof bmi)
+    let x = ""
     if(isNaN(bmi)){
-        console.log(`What wrong with you!`);
+        x="What wrong with you!"
     }
     else if(bmi<18){
-        console.log(`${bmi} is too low`);
+        x="too low"
     }
     else if(bmi<24){
-        console.log(`${bmi} is mid`);
+        x="too mid"
     }
     else if(bmi<30){
-        console.log(`${bmi} is too high`);
+        x="too high"
     }
     else{
-        console.log(`you're off the scale`);
+        x="off the charts"
     }
+    
+    newele1.innerHTML=x
 })
